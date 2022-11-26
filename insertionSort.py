a5d57a4b79175f36241279c735bac55a2b4a91e2
Md Antor <md.antor1155@gmail.com>
2022-11-27 01:53:26 +0600

    insertion sort practiced many tiems

--- insertionSort.py
+++ insertionSort.py
@@ -1,29 +1,17 @@
-# Insertion sort in Python
-
-
 def insertionSort(array):
-
     for step in range(1, len(array)):
         key = array[step]
+
         j = step - 1
-        
-        # Compare key with each element on the left of it until an element smaller than it is found
-        # For descending order, change key<array[j] to key>array[j].        
-        while j >= 0 and key < array[j]:
-            array[j + 1] = array[j]
-            j = j - 1
-        
-        # Place key at after the element just smaller than it.
-        array[j + 1] = key
+        while j>=0 and key < array[j]:
+            array[j+1] = array[j]
+            array[j] = key 
+            j = j-1
+
+        array[j +1] = key
 
 
-data = [9, 5, 1, 4, 3]
-insertionSort(data)
-print('Sorted Array in Ascending Order:')
-print(data)
-
-
-a = [2, 4, 5, 3, 3, 2]
+a = [10,9,8,7,6,5,4,3,2,5,1,4,7,5,8]
 insertionSort(a)
 print(a)
-print("hello world")
+        

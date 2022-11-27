f44341122bcef02d2ff333eee67c833fcd954d4b
Md Antor <md.antor1155@gmail.com>
2022-11-28 00:44:08 +0600

    binary search and insertionsort together

--- binarySearch.py
+++ binarySearch.py
@@ -1,22 +1,41 @@
-def binarySearchHelper(array, ele, left, right):
+
+def insertionSort(array):
+    for i in range(1, len(array)):
+        ele = array[i]
+
+        j = i-1
+        while j >=0 and ele < array[j]:
+            array[j+1] = array[j]
+            j = j-1
+        
+        array[j+1] = ele 
+
+
+def binarysearchHelper(array, ele, left, right):
     if(left > right):
         return False
     else:
-        mid = (left + right) //2
-
-        if array[mid] == ele:
+        mid = (left + right)//2
+        if(ele == array[mid]):
             return mid
-        elif array[mid] < ele:
-            return binarySearchHelper(array, ele, mid + 1, right)
+        elif(ele<array[mid]):
+            return binarysearchHelper(array, ele, left, mid-1)
         else:
-            return binarySearchHelper(array, ele,left, mid -1 )
-
+            return binarysearchHelper(array, ele, mid +1, right)
+        
 
 def binarySearch(array, ele):
-    return binarySearchHelper(array, ele, 0, len(array)+1)
+    if(ele < array[0] or ele > array[len(array)-1]):
+        return False
+    else:
+        return binarysearchHelper(array, ele, 0, len(array)-1)
 
 
-a = [1,3,5, 6, 7, 8, 19]
-k = binarySearch(a, 1)
+a = [5, 6, 3, 43, 45, 303, 33, 25, 2]
+print(a)
+insertionSort(a)
+print("sorted a:", a)
 
-print("the element is at position",k)
+k = binarySearch(a, 6)
+
+print("ther required element is at position ",k)

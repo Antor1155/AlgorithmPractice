473cc4f66ed77b00e9a93b98ef0c323e9bd5c62c
Md Antor <md.antor1155@gmail.com>
2022-11-28 16:34:43 +0600

    mergesort

--- mergeSort.py
+++ mergeSort.py
@@ -2,43 +2,52 @@ def merge(array, left, mid, right):
     i = left
     j = mid + 1
 
-    output_list = []
-    while (i <= mid or j <=right):
-        if(i <= mid and j <= right):
-            if(array[i]< array[j]):
-                output_list.append(array[i])
-                i = i + 1
-            else:
-                output_list.append(array[j])
+
+    tmp_array=[]
+    while(i <= mid or j <= right):
+        if(i<=mid and j<=right):
+            if(array[i]> array[j]):
+                tmp_array.append(array[j])
                 j = j+1
+            else:
+                tmp_array.append(array[i])
+                i = i+1
         elif(i <= mid):
-            output_list.append(array[i])
+            tmp_array.append(array[i])
             i = i+1
         else:
-            output_list.append(array[j])
-            j = j+1
-        
-    for k in range(left, right+1):
-        array[k] = output_list[k -left]
+            tmp_array.append(array[j])
+            j = j +1
     
+    for k in range(left, right+1):
+        array[k] = tmp_array[k -left]
     return
-        
 
-def mergeSort(array, left, right):
-    if(left== right):
+
+def mergeSortHelper(array, left, right):
+    if(left == right):
         return
     elif(left + 1 == right):
-        if(array[left]> array[right]):
+        if(array[left] > array[right]):
             array[left], array[right] = array[right], array[left]
-            
+        return
     else:
-        mid = (left + right) //2
-        mergeSort(array, left, mid)
-        mergeSort(array, mid + 1, right)
-
+        mid = (left + right)//2
+        mergeSortHelper(array, left, mid)
+        mergeSortHelper(array, mid+1, right)
         merge(array, left, mid, right)
 
-a = [2,5,8,3,6,9,1,4,7,10]
 
-mergeSort(a, 0, len(a)-1)
+
+
+
+def mergeSort(array):
+    if(len(array)<=1):
+        return
+    else:
+        mergeSortHelper(array, 0, len(array)-1)
+
+a = [2, 55, 8]
+
+mergeSort(a)
 print(a)

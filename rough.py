2a73286d593f2ccfa034afdb64b145a43271a567
Md Antor <md.antor1155@gmail.com>
2022-12-30 04:44:35 +0600

    updated and practiced the rough.py page and tried lamba, max of tuple using any particular key

--- rough.py
+++ rough.py
@@ -3,4 +3,36 @@ seq2 = ['a', 'b', 'c', 'd', 'e', 'f']
 
 a = [seq2[2 * j] for j in range(len(seq2)//2)]
 
-print(a)
+
+# Python program to find if there are
+# two elements with given sum
+ 
+# function to check for the given sum
+# in the array
+ 
+ 
+def printPairs(arr, arr_size, sum):
+ 
+    # Create an empty hash map
+    # using an hashmap allows us to store the indices
+    hashmap = {}
+ 
+    for i in range(0, arr_size):
+        temp = sum-arr[i]
+        if (temp in hashmap):
+            print('Yes')
+            return
+        hashmap[arr[i]] = i
+    print("No")
+ 
+ 
+# driver code
+A = [1, 4, 45, 6, 10, 8]
+n = 16
+printPairs(A, len(A), n)
+
+
+
+tuple_list = [(200, 1), (300, 5), (500, 1)]
+print(max(tuple_list, key=lambda item: item[1]))
+print(tuple_list)

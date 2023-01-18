372da39032baf993706e78e635c8153ac4ae6fd7
Md Antor <md.antor1155@gmail.com>
2023-01-19 02:08:06 +0600

    all test case passed

--- others/map_shortest_path_with_modify.py
+++ others/map_shortest_path_with_modify.py
@@ -1,6 +1,7 @@
 
 from collections import deque
-# modify array being safe in try function 
+# modify list being safe in try function
+# the array itself carry the weight and prent's position  
 def safe_mod(array, h, w, q, d):
     try:
         # up 
@@ -34,14 +35,11 @@ def safe_mod(array, h, w, q, d):
     except:
         pass
 
-# ***************safe mod from end **************************
 
 
-# ****************************************
+# this function check for adjacent wall and
+# any path adjacent to the wall with which corrent path might connect
 def safe_check(array,h, w, present_d):
-    # --- solved:::: safe_check bug where h-2 or w-2 were checking last elemetn of list  -1 or -2 **** h-1 also can cause same problem 
-    # --solved :: checking all four values and returing the best one i am not checking all four values, if one pass, then just returning it
-    # didn't raise any unexpected errors while verify 
     updated_d = present_d
     try:
         # up 2 step
@@ -128,9 +126,9 @@ def safe_check(array,h, w, present_d):
     return (False, None) if updated_d == present_d else(True, updated_d)
 
 
-
+# this function could be inserted in safe_mod function but it would look complicated
+# this function used when start and end is connected but to find better way
 def safe_mod_from_end(array, h, w, q, d, previous_distance):
-
     try:
         # up 
         if array[h-1][w][2] > 0 and h-1 > -1:
@@ -166,32 +164,43 @@ def safe_mod_from_end(array, h, w, q, d, previous_distance):
     return safe_check(array, h, w, previous_distance)
 
 
-
-
-
-
-
-
-
-
 def solution(map):
     # Your code here
     q = deque()
     map[0][0] = [0, 0, 1]
     q.append([0, 0, 1])
 
+    # bfs form start to end 
     while q:
         h, w, d= q.popleft()
         safe_mod(map, h, w, q, d+1)
     
-    # **************************************this part fixed **********************
-    present_d = map[len(map)-1][len(map[0])-1][2]
-
-    map[len(map)-1][len(map[0])-1] = [len(map)-1, len(map[0])-1, -1]
+    
     q.append([len(map)-1, len(map[0])-1, -1])
+    present_d = (len(map)-1) * (len(map[0])-1)
+
+    # to check if start and end is connected of not
+    try:
+        present_d = map[len(map)-1][len(map[0])-1][2]
+    except:
+
+        # if not connected bfs form end toward start while checkign any 1 step far path from start
+        map[len(map)-1][len(map[0])-1] = [len(map)-1, len(map[0])-1, -1]
+        while q:
+            h, w, d= q.popleft()
+            safe_mod(map, h, w, q, d-1)
+            succed, updated_distance = safe_check(map, h, w, present_d)
+            if succed:
+                if updated_distance < present_d:
+                    present_d = updated_distance
+        
+        return present_d
+
+    # if try function succed , single bfs to find better path form end to start
+    map[len(map)-1][len(map[0])-1] = [len(map)-1, len(map[0])-1, -1]
+
     while q:
         h, w, d = q.popleft()
-        
 
         succed, updated_distance = safe_mod_from_end(map, h, w, q, d-1, present_d)
         if succed:
@@ -205,71 +214,39 @@ def solution(map):
 
 
 
-
-    # testing case when the maze is not solved or end is not reached 
-  
-    # if not(map[len(map)-1][len(map[0])-1]):
-    #     distance = 20 * 20
-    #     map[len(map) -1][ len(map[0])-1] = [len(map)-1, len(map[0])-1, -1]
-    #     # check upper one 
-    #     q.append([len(map)-1, len(map[0])-1, -1])
-    #     while q:
-    #         h, w, d= q.popleft()
-
-    #         succed, x = safe_check(map, h, w, d)
-    #         if succed:
-    #             if x < distance:
-    #                 distance = x
-
-            
-    #         safe_mod(map, h, w, q, d-1)
-
-    #     # return distance
-    #     return distance
-
-
-
-
-
-    # ********************************* this part fixed ****************************
-    # present_d = map[len(map)-1][len(map[0])-1][2]
-    # current_at = [len(map)-1, len(map[0])-1]
-
-    # while current_at != [0, 0, 1]:
-    #     succed, updated_distance = safe_check(map, current_at[0], current_at[1], map[len(map)-1][len(map[0])-1][2])
-    #     if succed:
-    #         # this line of code passed test 3 ******************
-    #         # before it was return and didn't pass test 3
-    #         # so there can be multiple way which will produce better result.
-    #         if updated_distance < present_d:
-    #             print("updated d:",updated_distance, " current at: ", current_at)
-    #             present_d = updated_distance
-    #     current_at = map[current_at[0]][current_at[1]]
-
-    # return present_d
-
-
 # confution: if there any case where is no way to elcape...... 
 
 # base1 ans = 7 
 base1 = [[0, 1, 1, 0], [0, 0, 0, 1], [1, 1, 0, 0], [1, 1, 1, 0]]
 # base2 ans = 11 
 base2 = [[0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]]
-# this case is for when no path connected need to bfs form end
+# this case is no path connected need to bfs form end
 # priority last 
-# a = [[0, 0, 0, 0, 0, 0],
-#      [1, 1, 1, 1, 1, 1],
-#      [0, 0, 0, 0, 0, 0], 
-#      [0, 1, 1, 1, 1, 1],
-#      [0, 0, 0, 0, 1, 0],
-#      [0, 1, 1, 1, 1, 1], 
-#      [0, 0, 0, 0, 0, 0]]
+# result = 20
+a4= [[0, 0, 0, 0, 0, 0],
+     [1, 1, 1, 1, 1, 1],
+     [1, 1, 1, 1, 0, 0], 
+     [0, 0, 0, 0, 0, 1],
+     [0, 0, 0, 0, 1, 0],
+     [0, 1, 1, 1, 1, 1], 
+     [0, 0, 0, 0, 0, 0]]
+
+
+# where multiple way form a to b:
+# result = 14
+a3 = [[0, 0, 0, 0, 0, 0],
+     [1, 1, 1, 1, 0, 1],
+     [1, 1, 1, 1, 0, 0], 
+     [0, 0, 0, 0, 0, 1],
+     [0, 0, 0, 0, 1, 0],
+     [0, 1, 1, 1, 1, 1], 
+     [0, 0, 0, 0, 0, 0]]
 
 # solved: priority 1 
 # for corner case i didn't think of which may join conrer
 # just need to change in safe check **********************
 # ans = 11 
-a = [[0, 0, 0, 0, 0, 0],
+a2 = [[0, 0, 0, 0, 0, 0],
      [0, 1, 1, 1, 1, 0],
      [1, 0, 0, 0, 0, 0],
      [1, 0, 1, 1, 1, 1],
@@ -279,15 +256,17 @@ a = [[0, 0, 0, 0, 0, 0],
 # solved: priority 2
 # for another case where bfs for last elemetn may solve the question
 # expected ans = 12
-# a = [[0, 0, 0, 0, 0, 0],
-#      [0, 1, 1, 1, 1, 0],
-#      [0, 1, 1, 1, 1, 0], 
-#      [1, 1, 1, 0, 0, 0],
-#      [0, 1, 1, 0, 1, 1],
-#      [0, 1, 1, 0, 1, 1], 
-#      [0, 0, 0, 0, 0, 0]]
+a1 = [[0, 0, 0, 0, 0, 0],
+     [0, 1, 1, 1, 1, 0],
+     [0, 1, 1, 1, 1, 0], 
+     [1, 1, 1, 0, 0, 0],
+     [0, 1, 1, 0, 1, 1],
+     [0, 1, 1, 0, 1, 1], 
+     [0, 0, 0, 0, 0, 0]]
 
-print("solution of a : " ,solution(a))
-# solution(a)
-for i in a:
-    print(i)
+print("12-- a1 bfs for end give new sol : " ,solution(a1))
+print("11-- a2 corner case : " ,solution(a2))
+print("14-- a3 multiple way start-end : " ,solution(a3))
+print("20-- a4 start and end not connected : " ,solution(a4))
+print("7 of base1 : " ,solution(base1))
+print("11 of base1 : " ,solution(base2))
